e97d6f176eed1294580f63af6103c3106419f2bc
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-03-07 22:04:40 +0800

    feat: 测试微信登陆码

--- fitness-backend/src/app.service.ts
+++ fitness-backend/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { HttpService } from '@nestjs/axios'
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {
+
+  }
   getHello(): string {
     return 'Hello World!';
+  }
+  async testApi(code: string) {
+    const res = await this.httpService.axiosRef.get('https://api.weixin.qq.com/sns/jscode2session', {
+      params: {
+        appid: "wxa3649f39d6ff7f0e",
+        secret: "f7fe79dd861b6ea764db4bd125aab36a",
+        js_code: code,
+        grant_type: "authorization_code",
+      }
+    })
+    return res.data
   }
 }
