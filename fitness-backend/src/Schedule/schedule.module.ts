516f8abd9a13c5466ca0f11007dbc56cfdb6a44f
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-16 16:44:16 +0800

    fix: 文件module单独抽取

--- fitness-backend/src/Schedule/schedule.module.ts
+++ fitness-backend/src/Schedule/schedule.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { ScheduleController } from './schedule.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Schedule, User } from '../entities'
+import { Schedule, User, Media } from '../entities';
+import { FileModule } from '../fileModule'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Schedule, User])],
+  imports: [TypeOrmModule.forFeature([Schedule, User,Media]), FileModule],
   providers: [ScheduleService],
   controllers: [ScheduleController]
 })
--- fitness-backend/src/app.module.ts
+++ fitness-backend/src/app.module.ts
@@ -7,11 +7,11 @@ import { FoodModule } from './Food/food.module'
 import { HttpModule } from '@nestjs/axios'
 import { UserModule } from './User/user.module';
 import { DietModule } from './Diet/diet.module'
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer'
-import { getRandomFileName, acceptMimeTypes } from './utils'
-import { port, rootStaticDir } from './main'
-import { join } from 'path'
+import { ScheduleModule } from './Schedule/schedule.module';
+import { MediaModule } from './Media/media.module';
+import { FileModule } from './fileModule'
+
+
 
 @Module({
   imports: [
@@ -22,74 +22,23 @@ import { join } from 'path'
       username: 'root',
       password: 'zzxcxy666',
       database: 'mini-fitness',
-      autoLoadEntities: true,
       synchronize: true,
       verboseRetryLog: true,
       retryAttempts: 3,
       retryDelay: 3000,
-      timezone: 'Z'
+      timezone: 'Z',
+      autoLoadEntities: true
+      // entities: [
+      //   __dirname + '/entities.ts',
+      // ],
     }),
     FoodModule,
     HttpModule,
     UserModule,
     DietModule,
-    MulterModule.register({
-      fileFilter(req, file, cb) {
-        const type = file.mimetype.split('/')[0];
-        if (!acceptMimeTypes.includes(type)) {
-          cb(
-            new Error(
-              `暂不支持${file.originalname.slice(
-                file.originalname.lastIndexOf('.') + 1,
-              )}类型的文件`,
-            ),
-            false,
-          );
-          return;
-        }
-        // 解决中文名乱码的问题 latin1 是一种编码格式
-        file.originalname = Buffer.from(file.originalname, 'latin1').toString(
-          'utf8',
-        );
-        cb(null, true);
-      },
-      storage: diskStorage({
-        //destination为函数时，需要手动创建相应的目录
-        destination(
-          req,
-          file: Express.Multer.File & Record<'assetType', string>,
-          cb,
-        ) {
-          const { mimetype } = file;
-          const type = mimetype.split('/')[0];
-          let assetType = '';
-          let finalDir = '';
-          switch (type) {
-            case 'image':
-              assetType = 'images';
-              break;
-            case 'video':
-              assetType = 'videos';
-              break;
-            default:
-              assetType = 'files';
-          }
-          finalDir = join(rootStaticDir, assetType);
-          file.assetType = assetType;
-          cb(null, finalDir);
-        },
-        filename(
-          req,
-          file: Express.Multer.File & Record<'url' | 'assetType', string>,
-          cb,
-        ) {
-          const { originalname } = file;
-          const name = getRandomFileName(originalname);
-          file.url = `http://localhost:${port}/assets/${file.assetType}/${name}`;
-          cb(null, name);
-        },
-      }),
-    }),
+    ScheduleModule,
+    MediaModule,
+    FileModule
   ],
   controllers: [AppController],
   providers: [AppService],
