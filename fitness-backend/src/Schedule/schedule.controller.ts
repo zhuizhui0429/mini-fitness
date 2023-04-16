13e121c41fa6dbc8acdef9146c9c7628f5c7790a
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-16 16:46:07 +0800

    feat: 完成计划模块的基础业务功能搭建

--- fitness-backend/src/Schedule/schedule.controller.ts
+++ fitness-backend/src/Schedule/schedule.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Query, UseInterceptors, UploadedFile, UploadedFiles, SetMetadata, Get } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { ScheduleService } from './schedule.service';
+import type { createScheduleParamsType } from './schedule.service';
+import { ApiOperation, ApiTags, ApiQuery } from '@nestjs/swagger';
 
-@Controller('schedule')
-export class ScheduleController {}
+@Controller()
+@ApiTags('schedule')
+export class ScheduleController {
+
+    constructor(private readonly scheduleService: ScheduleService) { }
+
+    @Post('/createSchedule')
+    @ApiOperation({ description: '新建健身计划' })
+    @SetMetadata('successMessage', '新建计划成功')
+    @UseInterceptors(FileInterceptor('file'))
+    async createSchedule(@UploadedFile() poster: Express.Multer.File & { url: string }, @Body() restInfo: Omit<createScheduleParamsType, 'poster'>) {
+        console.log('封面poster', poster)
+        console.log('其他info', restInfo)
+        const { url } = poster
+        const res = await this.scheduleService.createSchedule({ ...restInfo, poster: url })
+        return res
+    }
+
+    @Get('/getAllSchedule')
+    @ApiOperation({ description: '查询某个用户创建的所有健身计划' })
+    @SetMetadata('successMessage', '获取所有计划成功')
+    @ApiQuery({ name: 'userId', description: '用户id' })
+    async getAllSchedule(@Query('userId') userId: number) {
+        return await this.scheduleService.getAllSchedule(userId)
+    }
+
+    @Post('/addMedia')
+    @ApiOperation({ description: '为健身计划上传图片或视频' })
+    @SetMetadata('successMessage', '添加媒体成功')
+    @UseInterceptors(FilesInterceptor('medias'))
+    async addMedia(@UploadedFiles() medias: (Express.Multer.File & { url: string })[], @Body('scheduleId') scheduleId: number) {
+        const res = await this.scheduleService.addMedia({
+            scheduleId,
+            medias: medias.map(({ url, mimetype, originalname }) => ({
+                url,
+                type: mimetype,
+                fileName: originalname
+            }))
+        })
+        return res
+    }
+
+    @Get('/getMediasOfSchedule')
+    @ApiOperation({ description: '获取某个计划下的所有媒体资源' })
+    @SetMetadata('successMessage', '获取计划媒体成功')
+    @ApiQuery({ name: 'scheduleId', description: '计划id' })
+    async getMediasOfSchedule(@Query('scheduleId') scheduleId: number) {
+        return await this.scheduleService.getMediasOfSchedule(scheduleId)
+    }
+}
--- fitness-backend/src/Schedule/schedule.service.ts
+++ fitness-backend/src/Schedule/schedule.service.ts
@@ -1,4 +1,77 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DataSource } from 'typeorm';
+import { getDataSource } from '../db';
+import { User, Media, Schedule } from '../entities';
+import { formatDate } from '../utils';
 
 @Injectable()
-export class ScheduleService {}
+export class ScheduleService {
+    dataSource: DataSource
+    constructor(
+        @InjectRepository(Schedule) private scheduleRepository: Repository<Schedule>,
+        @InjectRepository(User) private userRepository: Repository<User>,
+        @InjectRepository(Media) private mediaRepository: Repository<Media>
+    ) {
+        getDataSource().then(db => this.dataSource = db)
+    }
+
+    async createSchedule(data: createScheduleParamsType) {
+        const { title, description, poster, userId } = data
+        const scheduleEntity = await this.scheduleRepository.save({ title, description, poster })
+        await this.dataSource.createQueryBuilder()
+            .relation(Schedule, 'creator')
+            .of(scheduleEntity)
+            .set(userId)
+        return scheduleEntity
+    }
+
+    async getAllSchedule(userId: number) {
+        return await this.dataSource.createQueryBuilder()
+            .relation(User, 'schedules')
+            .of(userId)
+            .loadMany<Schedule>()
+    }
+
+    async addMedia(data: addMediaParamsType) {
+        const { medias, scheduleId } = data
+        const mediaEntities = await this.mediaRepository.save(medias.map(media => ({ ...media, date: formatDate(new Date()) })))
+        return await this.dataSource.createQueryBuilder()
+            .relation(Schedule, 'medias')
+            .of(scheduleId)
+            .add(mediaEntities)
+    }
+
+    async getMediasOfSchedule(scheduleId: number) {
+        const medias = await this.dataSource.getRepository(Media)
+            .createQueryBuilder('media')
+            .where('media.scheduleId = :scheduleId', { scheduleId })
+            .orderBy('media.date', 'DESC')
+            .getMany()
+        const data: Array<{ date: string, medias: Array<Pick<Media, 'fileName' | 'type' | 'url'>> }> = []
+        medias.forEach(({ url, type, fileName, date }) => {
+            const target = data.find(it => it.date === date)
+            const media = { url, type, fileName }
+            if (!target) {
+                data.push({
+                    date: date,
+                    medias: [media]
+                })
+                return
+            }
+            target.medias.push(media)
+        })
+        return data
+    }
+}
+
+export interface createScheduleParamsType extends Pick<Schedule, 'title' | 'description' | 'poster'> {
+    userId: number
+}
+
+export interface addMediaParamsType {
+    medias: Array<Omit<Media, 'id' | 'schedule' | 'date'>>,
+    scheduleId: number
+}
+
+
