e046de4d8edb679854d9b0132e0fe42adda37eb2
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-17 09:35:38 +0800

    fix: 分包优化体积

--- fitness-front/src/app.config.ts
+++ fitness-front/src/app.config.ts
@@ -1,11 +1,23 @@
 export default defineAppConfig({
   pages: [
-    'pages/record/record',
-    'pages/add/add',
-    'pages/dietDetail/dietDetail',
-    'pages/index/index',
     'pages/schedule/schedule',
-    'pages/weightManage/weightManage',
+    'pages/createSchedule/createSchedule',
+    'pages/scheduleDetail/scheduleDetail',
+    'pages/record/record',
+  ],
+  subpackages: [{
+    root: 'packageDiet',
+    pages: [
+      'pages/add/add',
+      'pages/dietDetail/dietDetail'
+    ]
+  },
+  {
+    root: 'packageWeight',
+    pages: [
+      'pages/weightManage/weightManage'
+    ]
+  }
   ],
   window: {
     backgroundTextStyle: 'light',
@@ -19,24 +31,17 @@ export default defineAppConfig({
     borderStyle: 'white',
     list: [
       {
+        pagePath: 'pages/schedule/schedule',
+        text: '计划',
+        iconPath: './assets/schedule.png',
+        selectedIconPath: './assets/schedule_active.png'
+      },
+      {
         pagePath: 'pages/record/record',
         text: '记录',
         iconPath: './assets/record.png',
         selectedIconPath: './assets/record_active.png'
       },
-      {
-        pagePath: 'pages/index/index',
-        text: '首页',
-        iconPath: './assets/index.png',
-        selectedIconPath: './assets/index_active.png'
-      },
-
-      {
-        pagePath: 'pages/schedule/schedule',
-        text: '计划',
-        iconPath: './assets/schedule.png',
-        selectedIconPath: './assets/schedule_active.png'
-      }
     ]
 
   }
